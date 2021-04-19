1e3175fada1e0a0896f29fa319c3d4bd9aa548cc
BASYH <binendaa@yandex.ru>
2021-04-20 04:14:19 +0500

    лаба3 задание2

---
+++ getotziv.php
@@ -0,0 +1,16 @@
+﻿<?php
+$schiotchik=0;
+$xml = simplexml_load_file("otziv.xml");
+$cn = count($xml->otziv);
+        for($i = 0; $i <= $cn; $i++){
+          $schiotchik++;
+          echo("<br>");
+          echo("<section class='otz'>");
+       echo "Имя: ".$xml->otziv[$i]->name . "<br>";
+       echo "Почта: ".$xml->otziv[$i]->email . "<br>";
+       echo "Отзыв: ".$xml->otziv[$i]->otz . "<br>";
+       echo("</section>");
+       if($schiotchik==$cn)break;
+        }
+
+?>
---
+++ otziv.php
@@ -0,0 +1,31 @@
+﻿<?php
+
+$tek=$_GET["tek"];
+$name=$_GET["name"];
+$email=$_GET["email"];
+$otz=$_GET["otz"];
+
+$xml = simplexml_load_file("otziv.xml"); 
+
+$cs = $xml->addChild('otziv','');
+$cs->addChild("name", $name);
+$cs->addChild("email", $email);
+$cs->addChild("otz", $otz);
+$done = $xml->asXML("otziv.xml");
+$schiotchik=0;
+$cn = count($xml->otziv);
+$xml = simplexml_load_file("otziv.xml");
+        for($i = 0; $i <= $cn; $i++){
+          $schiotchik++;
+          echo("<br>");
+          echo("<section class='otz'>");
+       echo "Имя: ".$xml->otziv[$i]->name . "<br>";
+       echo "Почта: ".$xml->otziv[$i]->email . "<br>";
+       echo "Отзыв: ".$xml->otziv[$i]->otz . "<br>";
+       echo("</section>");
+       if($schiotchik==$cn)break;
+        }
+
+
+
+?>
